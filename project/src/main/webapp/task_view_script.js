9ffabd3f22dd11fed1c5845aac80c713ce3f6206
Tanmay Bansal <tanmay17268@iiitd.ac.in>
2020-08-10 17:32:48 +0530

    Add task_view HTML/JS

---
+++ project/src/main/webapp/task_view_script.js
@@ -0,0 +1,16 @@
+function viewTaskDetails() {
+  var queryParams = new URLSearchParams(window.location.search);
+  queryParams = queryParams.toString();
+  fetch('/task?' + queryParams).then((response) => {
+      return response.json();
+  }).then((response) => {
+      const taskContainer = document.getElementById('task-container');
+      taskContainer.innerHTML = '<h1>' + response["title"] + '</h1>';
+      taskContainer.innerHTML += '<p>' + response["details"] + "</p>";
+      taskContainer.innerHTML += '<p><strong>Location</strong>: ' + response["address"] + "</p>";
+      var deadline = new Date(response["deadline"]).toUTCString();
+      taskContainer.innerHTML += '<p><strong>Task Deadline</strong>: ' + deadline + "</p>";
+      taskContainer.innerHTML += '<p><strong>Created By</strong>: ' + response["creatorId"] + "</p>";
+      taskContainer.innerHTML += '<p><strong>Compensation</strong>: ' + response["compensation"] + "</p>";
+  })
+}
