4fa3dbe5edc1bf5d49c449cfa2dad0b39d68b4d7
Arijit Saha <arijitbiley@gmail.com>
2020-08-13 11:08:42 +0530

    Add cursor and PAGE_SIZE to List Servlet

--- project/src/main/java/com/google/sps/servlets/TaskListServlet.java
+++ project/src/main/java/com/google/sps/servlets/TaskListServlet.java
@@ -1,18 +1,23 @@
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.Cursor;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.QueryResultList;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.sps.data.Task;
+import com.google.sps.data.User;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -24,8 +29,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
 /** Servlet facilitating listing of tasks. */
-@WebServlet(urlPatterns = {"/task/all", "/task/created", "/task/assigned"})
+@WebServlet(urlPatterns = {"/task/all", "/task/created", "/task/assigned", "/task/completed"})
 public class TaskListServlet extends HttpServlet {
+  static final int PAGE_SIZE = 20;
+  private final DatastoreService datastore;
+
+  public TaskListServlet() {
+    datastore = DatastoreServiceFactory.getDatastoreService();
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -37,31 +48,63 @@ public class TaskListServlet extends HttpServlet {
       return;
     }
 
-    // Prepare the Query
+    // Define the Query
     Query query = new Query("Task").addSort("creationTime", SortDirection.DESCENDING);
+    FetchOptions fetchOptions = FetchOptions.Builder.withLimit(PAGE_SIZE);
+
+    // If this servlet is passed a cursor parameter, use it.
+    String startCursor = request.getParameter("cursor");
+    if (startCursor != null) {
+      fetchOptions.startCursor(Cursor.fromWebSafeString(startCursor));
+    }
+
+    // Add filters based on URI (created, assigned, completed)
     if (uriInfo.equals("/task/created")) {
-      Filter creatorFilter = new FilterPredicate("creatorId", FilterOperator.EQUAL, 0);
+      // set filters for created tasks
+      User loggedInUser = User.getUserFromEmail(userService.getCurrentUser().getEmail());
+
+      Filter creatorFilter = new FilterPredicate("creatorId", FilterOperator.EQUAL,
+                                                 loggedInUser.getId());
       query.setFilter(creatorFilter);
+    } else if (uriInfo.equals("/task/assigned")) {
+      // set filters for assigned tasks
+      User loggedInUser = User.getUserFromEmail(userService.getCurrentUser().getEmail());
+
+      Filter assigneeFilter = new FilterPredicate("assigneeId", FilterOperator.EQUAL,
+                                                 loggedInUser.getId());
+      query.setFilter(assigneeFilter);
     } else if (uriInfo.equals("/task/completed")) {
-      //form filters for completed tasks
-      Filter creatorFilter = new FilterPredicate("creatorId", FilterOperator.EQUAL, -1);
-      query.setFilter(creatorFilter);
+      // set filters for completed tasks
+      User loggedInUser = User.getUserFromEmail(userService.getCurrentUser().getEmail());
+
+      Filter activeFilter = new FilterPredicate("active", FilterOperator.EQUAL, false);
+      Filter assigneeFilter = new FilterPredicate("assigneeId", FilterOperator.EQUAL,
+                                                 loggedInUser.getId());
+      query.setFilter(activeFilter).setFilter(assigneeFilter);
     }
 
     // Query the Datastore for the required tasks
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    PreparedQuery results = datastore.prepare(query);
+    QueryResultList<Entity> results;
+    try {
+      results = datastore.prepare(query).asQueryResultList(fetchOptions);
+    } catch (IllegalArgumentException e) {
+      // Illegal Cursor
+      return;
+    }
 
     List<Task> tasks = new ArrayList<>();
-    for (Entity entity : results.asIterable()) {
+    for (Entity entity : results) {
       Task task = Task.getTaskFromDatastoreEntity(entity);
       tasks.add(task);
     }
 
     // Send the JSON as the response
+    JsonObject json = new JsonObject();
     Gson gson = new Gson();
+    json.addProperty("nextCursor", results.getCursor().toWebSafeString());
+    json.add("tasks", gson.toJsonTree(tasks));
     response.setContentType("application/json;");
-    response.getWriter().println(gson.toJson(tasks));
+    response.getWriter().println(json.toString());
   }
 
   @Override
